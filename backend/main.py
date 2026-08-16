844cd4d8a3491b99b63075614fb88a9a100c3be1
Ruhaiba Ahmad <ruhaiba001@gmail.com>
2026-08-16 02:55:11 -0400

    Allow production frontend

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,10 @@ app = FastAPI(title="Apricity")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://apricity-teal.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
